3096719378728c57980edf36e7054222b9334479
yama-dev <tatsuya.yamamoto69@gmail.com>
2018-09-14 22:45:29 +0900

    modify: update version text.

--- src/js-calendar-module.js
+++ src/js-calendar-module.js
@@ -1,7 +1,7 @@
 /*!
  * JS CALENDAR_MODULE (JavaScript Library)
  *   js-calendar-module.js
- * Version 0.0.3
+ * Version 0.0.4
  * Repository https://github.com/yama-dev/js-calendar-module
  * Author yama-dev
  * Licensed under the MIT license.
