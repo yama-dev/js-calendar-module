03dde103104887c46aaae0ed3a5b81f4db524931
yama-dev <tatsuya.yamamoto69@gmail.com>
2020-09-07 21:53:42 +0900

    adjust minify setting.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,24 @@
 const webpack = require('webpack');
+const TerserPlugin = require('terser-webpack-plugin');
+const pkg = require('./package.json');
+
+const comment = `/*!
+ * JS CALENDAR_MODULE (JavaScript Library)
+ *   ${pkg.name}
+ * Version ${pkg.version}
+ * Repository ${pkg.repository.url}
+ * Copyright ${pkg.author}
+ * Licensed ${pkg.license}
+ */
+/*! JS PARSE_MODULE (JavaScript Library)   js-parse-module.js Version 0.0.3 Repository https://github.com/yama-dev/js-parse-module Author yama-dev Licensed under the MIT license. */
+/*! calendar.js: inspired by the calendar module from Python Copyright(c) 2011 Luciano Ramalho <luciano@ramalho.org> MIT Licensed */`;
 
 const env = process.env.NODE_ENV;
+
+const webpackPlugEnv = new webpack.EnvironmentPlugin({
+  VERSION: pkg.version,
+  DEBUG: false
+});
 
 const config = {
   mode: env || 'development',
@@ -27,14 +45,36 @@ const config = {
                   {
                     modules: false
                   }
-              ]
+                ]
               ],
             }
           }
         ],
       }
     ]
-  }
+  },
+  plugins: [
+    webpackPlugEnv
+  ]
 };
+
+if(env === 'production'){
+  config.optimization = {
+    minimizer: [
+      new TerserPlugin({
+        extractComments: false,
+        terserOptions: {
+          output: {
+            preamble: comment,
+            comments: false,
+          },
+          compress: {
+            drop_console: true
+          }
+        },
+      }),
+    ],
+  }
+}
 
 module.exports = config;
